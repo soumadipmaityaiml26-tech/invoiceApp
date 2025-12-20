ef010317535470965ff110ce7b664e70a6ff5788
sayakghosh <sayakghosh2004sg@gmail.com>
2025-12-21 01:17:28 +0530

    url changes

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
-    "https://4unq8dl7a7.execute-api.ap-south-1.amazonaws.com/prod/api/v1",
+    "https://4k4kjvugib.execute-api.ap-south-1.amazonaws.com/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
